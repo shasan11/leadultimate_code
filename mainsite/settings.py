92ab41c4fe57c5e14d0629894d80260c08fad34d
Shasan Dhakal <63613238+shasan11@users.noreply.github.com>
2023-06-29 18:46:12 +0545

    Update settings.py

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -91,7 +91,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/tmp/8db789e316a6ed9/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
